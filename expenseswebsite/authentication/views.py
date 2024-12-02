f36f74828a79af6a3538f61c6cb4186a7c22317e
Ralphilou Tatoy <ralphilou24@gmail.com>
2024-12-02 01:04:25 -0800

    Registration UI, Feedback, Show/Hide complete

--- expenseswebsite/authentication/views.py
+++ expenseswebsite/authentication/views.py
@@ -3,6 +3,7 @@ from django.views import View
 import json
 from django.http import JsonResponse
 from django.contrib.auth.models import User
+from validate_email import validate_email
 
 # Create your views here.
 class UsernameValidationView(View):
@@ -17,7 +18,19 @@ class UsernameValidationView(View):
             return JsonResponse({'username_error':'username is in use. choose another one'}, status=409)
         return JsonResponse({'username_valid': True})
 
-    
+
 class RegistrationView(View):
     def get(self, request):
         return render(request, "authentication/register.html")
+    
+class EmailValidationView(View):
+    def post(self, request):
+        data=json.loads(request.body)
+        email=data['email']
+        # check if username is alphanumeric
+        if not validate_email(email):
+            return JsonResponse({'email_error':'Email is invalid'}, status=400)
+        # check if username is in database
+        if User.objects.filter(email=email).exists():
+            return JsonResponse({'email_error':'Email is in use. choose another one'}, status=409)
+        return JsonResponse({'email_valid': True})
--- expenseswebsite/expenseswebsite/static/js/register.js
+++ expenseswebsite/expenseswebsite/static/js/register.js
@@ -1,15 +1,73 @@
 // get user input from register.html
 const usernameField=document.querySelector('#usernameField');
-const feedbackArea=document.querySelector('.invalid-feedback')
+const feedbackArea=document.querySelector('.invalid-feedback');
+const emailField=document.querySelector('#emailField');
+const emailFeedbackArea=document.querySelector('.emailFeedBackArea');
+const usernamesuccessOutput = document.querySelector('.usernamesuccessOutput');
+const emailsuccessOutput = document.querySelector('.emailsuccessOutput');
+const showPasswordTog = document.querySelector('.showPasswordToggle');
+const passwordField=document.querySelector('#passwordField');
+
+const handleToggleInput = (e) => {
+    if(showPasswordTog.textContent==='SHOW'){
+        showPasswordTog.textContent = "'HIDE";
+        passwordField.setAttribute("type", "text");
+    }else{
+        showPasswordTog.textContent="SHOW"
+        passwordField.setAttribute("type", "password");
+    }
+    
+}
+
+showPasswordTog.addEventListener('click',  handleToggleInput);
+
+emailField.addEventListener('keyup', (e) =>{
+    // console.log('77777', 7777777)
+    const emailVal= e.target.value;
+
+    // email feedback of checking availability
+    emailsuccessOutput.style.display = "block"
+    emailsuccessOutput.textContent=`Checking ${emailVal}`
+
+    emailField.classList.remove('is-invalid'); //bootstrap
+    emailFeedbackArea.style.display="none";
+
+    if(emailVal.length > 0)
+    {
+        fetch('/authentication/validate-email', {
+                body: JSON.stringify({
+                    email: emailVal
+                }),
+                method: 'POST',
+            }
+        ).then(res => res.json())
+        .then(data =>{
+            // Once email is found, checking is gone
+            emailsuccessOutput.style.display = "none"
+
+            // show error in frontend
+            if(data.email_error){
+                emailField.classList.add('is-invalid'); //bootstrap
+                emailFeedbackArea.style.display="block";
+                emailFeedbackArea.innerHTML=`<p> ${data.email_error} </p>`;
+            }
+        });
+    }
+});
+
 // once user types something
 usernameField.addEventListener('keyup', (e) => {
-    console.log('77777', 7777777)
+    // console.log('77777', 7777777)
     const usernameVal= e.target.value;
 
-    // usernameField.classList.remove('is-invalid') //bootstrap
-    // feedbackArea.style.display="none"
-    usernameField.classList.remove('is-invalid') //bootstrap
-    feedbackArea.style.display="none"
+    // username feedback of checking availability
+    usernamesuccessOutput.style.display = "block"
+    usernamesuccessOutput.textContent=`Checking ${usernameVal}`
+
+    //Error is remove if not found inavailable
+    usernameField.classList.remove('is-invalid'); //bootstrap
+    feedbackArea.style.display="none";
+
     if(usernameVal.length > 0)
     {
         fetch('/authentication/validate-username', {
@@ -20,12 +78,15 @@ usernameField.addEventListener('keyup', (e) => {
             }
         ).then(res => res.json())
         .then(data =>{
+            // Once username is found, checking is gone
+            usernamesuccessOutput.style.display = "none"
+
             // show error in frontend
             if(data.username_error){
-                usernameField.classList.add('is-invalid') //bootstrap
-                feedbackArea.style.display="block"
-                feedbackArea.innerHTML=`<p> ${data.username_error} </p>`
+                usernameField.classList.add('is-invalid'); //bootstrap
+                feedbackArea.style.display="block";
+                feedbackArea.innerHTML=`<p> ${data.username_error} </p>`;
             }
         });
     }
-})
+});
